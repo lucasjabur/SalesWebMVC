3e7cf04e67f9b008cdd6c01dce6fd2d19c76a932
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-27 10:06:01 -0300

    NEW: Implementing Seeding Service

--- SalesWebMVC/SalesWebMVC/Startup.cs
+++ SalesWebMVC/SalesWebMVC/Startup.cs
@@ -11,6 +11,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
+using SalesWebMVC.Data;
 
 namespace SalesWebMVC
 {
@@ -38,14 +39,16 @@ namespace SalesWebMVC
 
             services.AddDbContext<SalesWebMVCContext>(options =>
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMVCContext"), builder => builder.MigrationsAssembly("SalesWebMVC")));
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment()) // caso esteja no perfil de desenvolvimento, que é o caso...
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed(); // ...o 'seedingService' irá chamar o método 'Seed()' para popular a base de dados.
             }
             else
             {
