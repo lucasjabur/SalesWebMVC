20cac0bd0027eb4c628e6595901fe39cfd371a74
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-28 07:54:37 -0300

    NEW: Implementing asynchronous operations using Tasks (async and await)

--- SalesWebMVC/SalesWebMVC/Services/DepartmentService.cs
+++ SalesWebMVC/SalesWebMVC/Services/DepartmentService.cs
@@ -1,4 +1,5 @@
-﻿using SalesWebMVC.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,9 +15,9 @@ namespace SalesWebMVC.Services
             _context = context;
         }
 
-        public List<Department> FindAll()
+        public async Task<List<Department>> FindAllAsync()
         {
-            return _context.Department.OrderBy(dep => dep.Name).ToList();
+            return await _context.Department.OrderBy(dep => dep.Name).ToListAsync();
         }
     }
 }
