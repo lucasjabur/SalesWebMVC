c89052b288b61a07d39fd525931f82217dc33dc6
Lucas Cardoso Jabur <lucas.cardoso.jabur@outlook.com>
2025-10-28 07:41:10 -0300

    NEW: Implementing data validation

--- SalesWebMVC/SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/SalesWebMVC/Models/Seller.cs
@@ -9,26 +9,34 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 5, ErrorMessage = "{0} size should be between {2} and {1}.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid email address!")]
         [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
-        
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
-        
+
+        [Required(ErrorMessage = "{0} required")]
+        [Range(1500.0, 100000.0, ErrorMessage = "{0} must go from {1} to {2}.")]
         [Display(Name = "Base Salary")]
         [DataType(DataType.Currency)]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
-        
-        public Department Department { get; set; }
 
         [Display(Name = "Department")]
         public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller() { }
